5b0c72c155b9f29a298137e6825846dd75ced885
supassara265 <124548110+supassara265@users.noreply.github.com>
2023-05-29 12:01:40 +0700

    Add files via upload

---
+++ food.js
@@ -0,0 +1,159 @@
+var food ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ครัวอัปษร",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.50153455963863,
+            13.755369881474564
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Supanniga Eating Room",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52747929262551,
+            13.723106745679317
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เรือนมัลลิการ์",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56309729262364,
+            13.725288744741135
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านขนิษฐา",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56338536067454,
+            13.741282312075484
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อรรถรส",
+          "level": "⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.57165329262688,
+            13.737770313428712
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sala Rattanakosin",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.49083902561483,
+            13.745365161730206
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หมี่ต้นโพธิ์",
+          "level": "⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.33443629262268,
+            7.9110410882965425
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "The Glass House Pattaya",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.88489980939323,
+            12.935611192103849
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ก๋วยจั๊บช้างม่อยตัดใหม่",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.99882108103577,
+            18.79010791928792
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " ราก (RARK Authentic Thai Cuisine)",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54390364380964,
+            13.679963701312303
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขียวไข่กา ",
+          "level": "⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            100.56307949159259,
+            13.745979735850042
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
